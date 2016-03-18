38f870c6d4343f02bf810823ef7569d890135e01
Aliza Pilisuk <apilisuk@gmail.com>
2016-03-18 10:39:04 -0700

    added problem three

--- README.md
+++ README.md
@@ -5,9 +5,16 @@ Enter sum(numOne, numTwo); in console.
 -This function takes two numerical parameters.
 -I then created a variable called sumTotal which adds the two parameters.
 -Finally the function enters a message in the console stating the two numerical values and their sum.  
-##Sum Function  
+##Multiply Function  
 Think of two numerical values to enter in function. Use these as numOne and numTwo.
 Enter multiply(numOne, numTwo); in console.  
 -This function takes two numerical parameters.
 -I then created a variable called multiplyProduct which multiplies the two parameters.
 -Finally the function enters a message in the console stating the two numerical values and their product.  
+##Sum and Multiply Function
+Think of three numerical values to enter in function. Use these as numOne, numTwo, and numThree.
+Enter sumAndMultiply(numOne, numTwo, numThree); in console.  
+-This function takes three numerical parameters and shows you the result of them all multiplied together and added together.
+-Commented out console.log();
+-Needed to return previous functions to call them in this function.
+-This function will return the sum and the product in an array and console.log what happened.
--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 function sum(numOne, numTwo){
   var sumTotal = numOne + numTwo;
-  console.log('The sum of ' + numOne + ' and ' + numTwo + ' is ' + sumTotal + '.');
+  return sumTotal;
+  //console.log('The sum of ' + numOne + ' and ' + numTwo + ' is ' + sumTotal + '.');
 }
 
 function multiply(multiplyNumOne, multiplyNumTwo) {
   var multiplyProduct = multiplyNumOne * multiplyNumTwo;
-  console.log('The product of ' + multiplyNumOne + ' and ' + multiplyNumTwo + ' is ' + multiplyProduct + '.');
+  return multiplyProduct;
+  //console.log('The product of ' + multiplyNumOne + ' and ' + multiplyNumTwo + ' is ' + multiplyProduct + '.');
+}
+
+function sumAndMultiply(sumAndMultiplyNumOne, sumAndMultiplyNumTwo, sumAndMultiplyNumThree){
+  var sumResult = sum(sum(sumAndMultiplyNumOne, sumAndMultiplyNumTwo), sumAndMultiplyNumThree);
+  var multiplyResult = multiply(multiply(sumAndMultiplyNumOne, sumAndMultiplyNumTwo), sumAndMultiplyNumThree);
+  var sumAndMultiplyArray = [sumResult, multiplyResult];
+  console.log(sumAndMultiplyNumOne + ' and ' + sumAndMultiplyNumTwo + ' and ' + sumAndMultiplyNumThree + ' sum to ' + sumResult + '.');
+  console.log('The numbers ' + sumAndMultiplyNumOne + ' and ' + sumAndMultiplyNumTwo + ' and ' + sumAndMultiplyNumThree + ' have a product of ' + multiplyResult + '.');
+  return sumAndMultiplyArray;
 }
